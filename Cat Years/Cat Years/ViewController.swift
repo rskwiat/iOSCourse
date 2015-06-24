0b1e476bf2e628f54cf614e71a1fd7c38ab9bf09
Robert Skwiat <rskwiat@gmail.com>
2015-06-24 15:37:24 -0400

    first iOS application, cat years

--- Cat Years/Cat Years/ViewController.swift
+++ Cat Years/Cat Years/ViewController.swift
@@ -1,6 +1,6 @@
-//
+ //
 //  ViewController.swift
-//  ExampleApp
+//  Cat Years
 //
 //  Created by Robert Skwiat on 6/24/15.
 //  Copyright (c) 2015 robertskwiat. All rights reserved.
@@ -9,19 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var userLabel: UILabel!
-    @IBAction func labelChangeButton(sender: UIButton) {
-        userLabel.text = "Button was tapped"
-    }
 
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var catsAgeInput: UITextField!
+    @IBAction func calculateAgeButton(sender: AnyObject) {
+
+        var enteredAge = catsAgeInput.text.toInt()
+
+        if enteredAge != nil {
+            var catYears = enteredAge! * 7
+            
+            outputLabel.text = "Your cat is: \(catYears) in cat years"
+        } else {
+            outputLabel.text = "Please enter an age."
+        }
+        
+     
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        //userLabel.text = "It works"
-        
-        //println("hello world")
     }
 
     override func didReceiveMemoryWarning() {
