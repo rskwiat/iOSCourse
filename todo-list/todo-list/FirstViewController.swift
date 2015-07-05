dacac5f161fe8d851c63dc6f7fc99abe6bea0f44
Robert Skwiat <rskwiat@gmail.com>
2015-07-05 16:02:42 -0400

    Basic to do list with two views

---
+++ todo-list/todo-list/FirstViewController.swift
@@ -0,0 +1,62 @@
+//
+//  FirstViewController.swift
+//  todo-list
+//
+//  Created by Robert Skwiat on 7/5/15.
+//  Copyright (c) 2015 Robert Skwiat. All rights reserved.
+//
+
+import UIKit
+
+var toDoList = [String]()
+
+class FirstViewController: UIViewController, UITableViewDelegate {
+    
+    
+
+    @IBOutlet var toDoListTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if NSUserDefaults.standardUserDefaults().objectForKey("toDoList") != nil {
+            toDoList = NSUserDefaults.standardUserDefaults().objectForKey("toDoList") as! [String]
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return toDoList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = toDoList[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+    
+        if editingStyle == UITableViewCellEditingStyle.Delete{
+            toDoList.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+            toDoListTable.reloadData()
+        }
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        toDoListTable.reloadData()
+    }
+    
+
+
+}
+
