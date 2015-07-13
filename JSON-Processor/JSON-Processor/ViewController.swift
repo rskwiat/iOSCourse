b3dcc14aed13af8a31ef47ad2a09855abdd5df72
Robert Skwiat <rskwiat@gmail.com>
2015-07-13 17:14:21 -0400

    creating a basic json processor

---
+++ JSON-Processor/JSON-Processor/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  JSON-Processor
+//
+//  Created by Robert Skwiat on 7/13/15.
+//  Copyright (c) 2015 robertskwiat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlPath = "http://www.telize.com/geoip"
+        let url = NSURL(string: urlPath)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+            if error != nil{
+                println(error)
+            } else{
+//                println(data)
+                let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+                
+                println(jsonResult["country_code"])
+                
+            }
+        })
+        
+        task.resume()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
