9c9a3af5a4ef1f4a8a5306489d71a1d9f7809583
Robert Skwiat <rskwiat@gmail.com>
2015-07-17 12:40:28 -0400

    commiting the table view controller for list of all the items

--- instagram clone/ParseStarterProject/TableViewController.swift
+++ instagram clone/ParseStarterProject/TableViewController.swift
@@ -7,11 +7,39 @@
 //
 
 import UIKit
+import Parse
 
 class TableViewController: UITableViewController {
+    
+    var usernames = [""]
+    var userids = [""]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var query = PFUser.query()
+        
+        query?.findObjectsInBackgroundWithBlock { (objects, errors) -> Void in
+            if let users = objects {
+                
+                self.usernames.removeAll(keepCapacity: true)
+                self.userids.removeAll(keepCapacity: true)
+                
+                for object in users {
+                    if let user = object as? PFUser {
+                        
+                        if user.objectId != PFUser.currentUser().objectId {
+                            self.usernames.append(user.username!)
+                            self.userids.append(user.objectId!)
+                        }
+                    }
+                }
+            }
+            self.tableView.reloadData()
+        }
+
+    
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -36,14 +64,14 @@ class TableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 3
+        return usernames.count
     }
 
   
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
 
-        cell.textLabel.text = "Hello"
+        cell.textLabel.text = usernames[indexPath.row]
         // Configure the cell...
 
         return cell
