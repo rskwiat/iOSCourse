30400bb904b6f9bc3a1d5fca429f406a1c66835f
Robert Skwiat <rskwiat@gmail.com>
2015-08-16 12:06:09 -0400

    adding in background for Flappy Bird clone app, getting an understanding of Physics

--- FlappyBird/FlappyBird/GameScene.swift
+++ FlappyBird/FlappyBird/GameScene.swift
@@ -11,10 +11,29 @@ import SpriteKit
 class GameScene: SKScene {
     
     var bird = SKSpriteNode()
+    var bg = SKSpriteNode()
     
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
+        var bgTexture = SKTexture(imageNamed: "img/bg.png")
+ 
+        var movebg = SKAction.moveByX(-bgTexture.size().width, y: 0, duration: 9)
+        var replacebg = SKAction.moveByX(bgTexture.size().width, y: 0, duration: 0)
+        var movebgFoverer = SKAction.repeatActionForever(SKAction.sequence([movebg,replacebg]))
+        
+        for var i:CGFloat=0; i<3; i++ {
+            
+            bg = SKSpriteNode(texture: bgTexture)
+            bg.position = CGPoint(x: bgTexture.size().width/2 + 900 * i, y: CGRectGetMidY(self.frame))
+            bg.size.height = self.frame.height
+            
+            bg.runAction(movebgFoverer)
+            
+            self.addChild(bg)
+            
+        }
         
         var birdTextture = SKTexture(imageNamed: "img/flappy1.png")
         var birdTexture2 = SKTexture(imageNamed: "img/flappy2.png")
@@ -27,12 +46,32 @@ class GameScene: SKScene {
         bird.position = CGPoint(x: CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
         bird.runAction(makeBirdFlap)
         
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height / 2)
+        bird.physicsBody?.dynamic = true
+        bird.physicsBody?.allowsRotation = false
+        
+        
         self.addChild(bird)
+        
+        var ground = SKNode()
+        ground.position = CGPointMake(0, 0)
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, 1))
+        ground.physicsBody?.dynamic = false
+        
+        self.addChild(ground)
+        
+        
+        
+
+        
 
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         /* Called when a touch begins */
+        
+        bird.physicsBody?.velocity = CGVectorMake(0,0)
+        bird.physicsBody?.applyImpulse(CGVectorMake(0,50))
         
     }
    
